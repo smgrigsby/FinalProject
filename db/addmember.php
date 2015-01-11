cb9ac37bb0bdeffd8038c97533b670a3a589f85e
Sarah Meister <sarahmgrigsby@gmail.com>
2015-01-11 10:57:31 -0800

    php > add new member

--- db/addmember.php
+++ db/addmember.php
@@ -2,4 +2,19 @@
 	$user="root";
 	$pass="root";
 	$dbh = new PDO('mysql:host=localhost;dbname=FinalProject;port=8889', $user, $pass);
+
+	if (filter_var(($_POST['memberemail']), FILTER_VALIDATE_EMAIL)){
+		$membername = $_POST['membername'];
+		$memberemail = $_POST['memberemail'];
+		$memberpassword = $_POST['memberpassword'];
+
+		$stmt = $dbh->prepare("INSERT INTO member (membername, memberemail, memberpassword) 
+			VALUES ('$membername', '$memberemail', 'memberpassword')");
+		$stmt->bindParam(':membername', $membername);
+		$stmt->bindParam(':memberemail', $memberemail);
+		$stmt->bindParam(':memberpassword', $memberpassword);
+		$stmt->execute();
+		header('Location: signup.html');
+		die();
+	}else{echo "<h2>Sorry this is not a valid email.</h2>";}
 ?>
