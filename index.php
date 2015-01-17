ff2644fff584f90b7d3d97fdce692e9439b39ef9
Sarah Meister <sarahmgrigsby@gmail.com>
2015-01-16 21:23:43 -0800

    restructured files

--- index.php
+++ index.php
@@ -23,12 +23,21 @@ if (version_compare(PHP_VERSION, '5.3.7', '<')) {
 // include the configs / constants for the database connection
 require_once("config/db.php");
 
-// load the registration class
-require_once("classes/Registration.php");
+// load the login class
+require_once("classes/Login.php");
 
-// create the registration object. when this object is created, it will do all registration stuff automatically
-// so this single line handles the entire registration process.
-$registration = new Registration();
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
 
-// show the register view (with the registration form, and messages/errors)
-include("views/register.php");
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+    include("views/logged_in.php");
+
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("views/not_logged_in.php");
+}
