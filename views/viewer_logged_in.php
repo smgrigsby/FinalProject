7492095e0fa3c87dbd2454f8c3d8ac74373b78ae
Sarah Meister <sarahmgrigsby@gmail.com>
2015-01-25 03:24:06 -0800

    created viewer account homepage

---
+++ views/viewer_logged_in.php
@@ -0,0 +1,85 @@
+
+<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Dashboard</title>
+	</head>
+	
+	<body>
+		<div id="homeview">
+			<header>
+				<ul>
+					<li><a href='#defaultview'></a></li>
+					<li><a href='#custpage_menu'><img src="assets/icon_cpage.png" height="50px" width="50px"/></a></li> 
+					<li><a href='#archive_menu'><img src="assets/icon_arch.png" height="50px" width="50px"/></a></li> 
+					<li><a href='#search_menu'><img src="assets/icon_sear.png" height="50px" width="50px"/></a></li> 
+					<li><a href='#settings_menu'><img src="assets/icon_cset.png" height="50px" width="50px"/></a></li> 
+				</ul>
+				<h1>BabyGrigsby</h1>
+			</header>
+
+		<!-- Default Tab, Empty Placeholder -->	
+			<div id="defaultview"></div>
+
+		<!-- Pages Tab -->	
+			<div id="custpage_menu" class="menu_view">
+
+				<?php echo 
+					'<ul>
+						<li><a href="#">Custom Page 1</a></li>
+						<li><a href="#">Custom Page 2</a></li>
+						<li><a href="#">Custom Page 3</a></li>
+						<li><a href="#">Custom Page 4</a></li>
+					</ul>' 
+				?>
+			</div>
+		<!-- Archives Tab -->	
+			<div id="archive_menu" class="menu_view">
+				<h3> Recent Posts </h3>
+				<h3> Recent Comments </h3>
+				<h3> Archives </h3>
+
+
+			</div>
+		<!-- Search Tab -->	
+			<div id="search_menu" class="menu_view">
+				<form action="search.php" method="POST">
+					<input class= "input_search" type="text">
+					<input class="submitBtn" type="submit" value="Search" />
+				</form>
+			</div>
+		<!-- Settings Tab -->	
+			<div id="settings_menu" class="menu_view">
+
+
+			</div>
+			<div class="user_greeting">
+				<h3> Welcome <?php echo $_SESSION['user_name']; ?>!</h3>
+				<a href='index.php?logout'>Logout</a> <!--"index.php?logout" is just my simplified form of "index.php?logout=true" -->
+			</div>
+		</div>
+
+		<footer>
+			<p> copyright 2014 BabyGrigsby </p>
+		</footer>
+<!-- END HTML, BEGIN LINKS AND FORMATTING -->
+    	<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+    	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    	
+    	<link href="css/main.css" rel="stylesheet">
+    	<?php echo '<style> #search_menu .submitBtn{ background-color: #'.$_SESSION['bgcolor'].'; }#custpage_menu a{font-weight: bold;color: #'.$_SESSION['bgcolor'].'; }</style>'?>
+
+    	
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:900' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+		<script>
+			$(function() {
+				$("#homeview").tabs();
+			
+			});
+		</script>
+
+	</body>
+</html>
